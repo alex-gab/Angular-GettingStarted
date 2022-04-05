5bd28bfc0bcc08a1bd874994fd2bc372f1cd9855
Alex Dragoi <dragoi.alex@gmail.com>
2022-04-05 10:23:28 +0300

    ch12

--- APM/src/app/products/product-detail.component.ts
+++ APM/src/app/products/product-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { IProduct } from './product';
 
 @Component({
@@ -10,12 +10,12 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = "Product Detail";
   product: IProduct | undefined;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     const id: number = Number(this.route.snapshot.paramMap.get("id"));
     this.pageTitle += `: ${id}`;
-    this.product =  {
+    this.product = {
       "productId": 1,
       "productName": "Leaf Rake",
       "productCode": "GDN-0011",
@@ -25,6 +25,10 @@ export class ProductDetailComponent implements OnInit {
       "starRating": 3.2,
       "imageUrl": "assets/images/leaf_rake.png"
     };
+  }
+
+  onBack(): void {
+    this.router.navigate(["/products"]);
   }
 
 }
